4302cdd0dc4d457ea0de9bab5e2ead26ddf231cd
jin <jincomeon@163.com>
2017-11-04 14:54:52 +0800

    add iconfont

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <indexHeader></indexHeader>
         <indexSwiper></indexSwiper>
         <indexScenicSwiper></indexScenicSwiper>
         <indexHot></indexHot>
@@ -7,12 +8,14 @@
 </template>
 
 <script>
+    import header from "./header/header.vue";
     import swiper from "./index-swiper.vue";
     import scenic from "./scenicSpot/index-scenic-controller.vue";
     import hot from "./hot/index-hot.vue";
     export default{
         name : "Home",
         components: {
+            indexHeader: header,
             indexSwiper: swiper,
             indexScenicSwiper: scenic,
             indexHot: hot
