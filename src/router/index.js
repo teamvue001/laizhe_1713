388b75ffc5e3a4a5fd649a1091d993b4d7b39a4b
tobe <2934208480@qq.com>
2017-11-11 19:54:18 +0800

    addPalacePage

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from "../pages/home/home.vue";
+import Palace from "../pages/list/imperialPalace/imperialPalace.vue"
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
       {
           path: '/',
           name: 'Home',
           component: Home
+      },
+      {
+          path: '/palace',
+          name: 'Palace',
+          component: Palace
       }
   ]
 })
