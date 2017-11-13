c504b541017c5401cf5fbb86bf5f9ddf3b6eac4e
yeluyang <793409550@qq.com>
2017-11-13 09:10:28 +0800

    change

---
+++ src/pages/comment/comment.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div>
+      <indexHeader :comment="comment" :commentList="commentList"></indexHeader>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    import Comment from "./ScenicSpotReview.vue";
+
+    export default{
+        name : "Comment",
+        components: {
+            indexHeader: Comment,
+        },
+        data() {
+            return {
+                comment :[],
+                commentList : []
+            }
+        },
+        mounted () {
+            axios.get("/static/index.json").then((res) => {
+               this.comment = res.data.data.comment;
+               this.commentList = res.data.data.commentList;
+               console.log(this.commentList)
+                console.log(this.comment)
+            })
+        }
+    }
+
+</script>
+
+<style>
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Comment from "../pages/comment/comment.vue";
 import Home from "../pages/home/home.vue";
 
 Vue.use(Router)
@@ -11,6 +11,11 @@ export default new Router({
           path: '/',
           name: 'Home',
           component: Home
+      },
+       {
+          path: '/comment',
+          name: 'Comment',
+          component: Comment
       }
   ]
 })
